9132372277643c82b5088d687c4a5ae02874ce83
Ujjwal Paul <devujjwal1989@gmail.com>
2018-05-21 15:13:24 +0530

    interim commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyCAP9xWHiGhJnH642Uq39Gk7aWstQvn3Qo",
+    authDomain: "bptshop-2018.firebaseapp.com",
+    databaseURL: "https://bptshop-2018.firebaseio.com",
+    projectId: "bptshop-2018",
+    storageBucket: "bptshop-2018.appspot.com",
+    messagingSenderId: "711894596370"   
   }
 };
